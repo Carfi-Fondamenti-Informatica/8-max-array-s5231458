46072888ee22aec740c6ddb47c71226ef2062033
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-12-11 16:32:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int dim;
+    cin>>dim;
+    float numeri [dim];
+    for (int i=0;i<dim;i++){
+        cin>>numeri[i];
 
-int main(){
-  return 0;
+    }
+    numeroMassimo(numeri, dim);
+    cout <<numeroMassimo(numeri, dim)<<endl;
+    return 0;
 }
